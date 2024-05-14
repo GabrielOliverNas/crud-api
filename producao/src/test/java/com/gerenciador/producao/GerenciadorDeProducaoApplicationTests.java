de7ddc3e53d7b5c4806d0d82365e79cca56149c7
gabriel oliveira do nascimento <gabrielolivernas@gmail.com>
2024-05-14 16:03:03 -0300

    Primeiro commit

---
+++ producao/src/test/java/com/gerenciador/producao/GerenciadorDeProducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.producao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeProducaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
